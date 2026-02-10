a1303a9299d309f0b98083c70a1e5bbed5d24b2d
Motojima <r.motojima5@gmail.com>
2026-02-10 21:07:09 +0900

    casestudyセクションの作成

--- js/script.js
+++ js/script.js
@@ -4,3 +4,28 @@ $("#js-drawer-icon").on("click", function (e) {
   $("#js-drawer-menu").toggleClass("is-checked");
   $("body").toggleClass("is-fixed");
 });
+
+const swiper = new Swiper("#js-case-study-swiper", {
+  // Optional parameters
+  // バグるためコメントアウト
+  // loop: true,
+  spaceBetween: 18,
+  width: 360,
+
+  breakpoints: {
+    768: {
+      width: 320,
+    }
+  },
+
+  // If we need pagination
+  pagination: {
+    el: "#js-case-study-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: "#js-case-study-next",
+    prevEl: "#js-case-study-prev",
+  },
+});
